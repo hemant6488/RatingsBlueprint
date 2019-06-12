6c27c518b7202c73e9a043924406398f2df5dde7
Hemant Kumar <hemant6488@gmail.com>
2019-06-12 21:56:42 +0530

    Dynamically changing ratings via ajax

--- static/rate.js
+++ static/rate.js
@@ -1,13 +1,20 @@
 jQuery(document).ready(function ($) {
-    var current_rating = Number($('input#loggedInUserRating').val())
-    $("#selected_rating").val(current_rating);
-	$(".selected-rating").empty();
-	$(".selected-rating").html(current_rating);
+    function populateStars(current_rating) {
+        $("#selected_rating").val(current_rating);
+        $(".selected-rating").empty();
+        $(".selected-rating").html(current_rating);
 
-    for (i = 1; i <= current_rating; ++i) {
-        $("#rating-star-" + i).toggleClass('btn-warning');
-        $("#rating-star-" + i).toggleClass('btn-default');
-	}
+        if (current_rating == 0) {
+           $('button[id^="rating-star"]').removeClass("btn-warning");
+           $('button[id^="rating-star"]').addClass("btn-default");
+        }
+
+        for (i = 1; i <= current_rating; ++i) {
+            $("#rating-star-" + i).toggleClass('btn-warning');
+            $("#rating-star-" + i).toggleClass('btn-default');
+        }
+    }
+    populateStars(Number($('input#loggedInUserRating').val()));
 
     function showError(message) {
         $("#message-display").text(message);
@@ -31,6 +38,7 @@ jQuery(document).ready(function ($) {
                 if (data.status == "failure") {
                     showError(data.message);
                 } else {
+                    getDetailedRatings();
                     hideError();
                 }
             }
@@ -47,10 +55,46 @@ jQuery(document).ready(function ($) {
             contentType: "application/json",
             data: JSON.stringify({userId: Number(userId), productId: Number(productId)}),
             success: function(data){
-                console.log(data)
+                if (data.status == "failure") {
+                    showError(data.message);
+                } else {
+                    getDetailedRatings();
+                    populateStars(0);
+                }
             }
         });
     }
+
+    function getDetailedRatings(){
+        var url = new URL(window.location.href);
+        var userId = url.searchParams.get("userId");
+        var productId = url.pathname.split('/')[3]
+        $.ajax({
+            type: "POST",
+            url: "/casaone/ratings/v1/getDetailed",
+            contentType: "application/json",
+            data: JSON.stringify({userId: Number(userId), productId: Number(productId)}),
+            success: function(data){
+                repaintRatingWidget(data.data);
+            }
+        });
+    }
+
+    function repaintRatingWidget(data){
+        $("#total-ratings").text(data.totalRatings + " Total Ratings");
+        $("#average-rating").text(data.averageRating);
+        $("#5-star-bar").css('width', (data.ratingsBreakdown["5"]).toString() + "%");
+        $("#4-star-bar").css('width', (data.ratingsBreakdown["4"]).toString() + "%");
+        $("#3-star-bar").css('width', (data.ratingsBreakdown["3"]).toString() + "%");
+        $("#2-star-bar").css('width', (data.ratingsBreakdown["2"]).toString() + "%");
+        $("#1-star-bar").css('width', (data.ratingsBreakdown["1"]).toString() + "%");
+        $("#5-star-text").text((data.ratingsBreakdown["5"]).toString() + "%");
+        $("#4-star-text").text((data.ratingsBreakdown["4"]).toString() + "%");
+        $("#3-star-text").text((data.ratingsBreakdown["3"]).toString() + "%");
+        $("#2-star-text").text((data.ratingsBreakdown["2"]).toString() + "%");
+        $("#1-star-text").text((data.ratingsBreakdown["1"]).toString() + "%");
+    }
+
 
 	$(".btnrating").on('click', (function (e) {
 
